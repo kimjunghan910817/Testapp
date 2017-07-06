b7f18b4bd889dbd4a2efa3ba1d4bc08e2bd1fa3b
kimjunghan <kimjunghan@ais-info.co.jp>
2017-07-06 09:27:28 +0900

    cccc

--- sample1/src/sample1/HelloApp.java
+++ sample1/src/sample1/HelloApp.java
@@ -4,6 +4,6 @@ public class HelloApp {
 
 	public static void main(String[] args){
 		MessageBean bean = new MessageBean();
-		bean.sayHello("SpringTest1222");
+		bean.sayHello("SpringTest12222222");
 	}
 }
